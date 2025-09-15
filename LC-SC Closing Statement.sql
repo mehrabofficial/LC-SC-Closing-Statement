cb442d77dadedd643cec3c70b9c97247bc999806
Md. Mehrab Hossain <86048413+mehrabofficial@users.noreply.github.com>
2025-09-15 16:59:42 +0600

    Create LC-SC Closing Statement.sql

---
+++ LC-SC Closing Statement.sql
@@ -0,0 +1,42 @@
+SELECT 
+    lc.COMPANY_SHORT_NAME AS SBU,
+    lb.SHORT_NAME AS BUYER,
+    bb.BRAND_NAME AS BUYER_BRAND,
+    csc.INTERNAL_FILE_NO AS FILE_REF, 
+    cel.EXPORT_LC_NO AS LC_NUMBER, 
+    csc.CONTRACT_NO AS "SC/MASTER LC NO."  
+
+FROM 
+    WO_PO_DETAILS_MASTER wpdm
+JOIN 
+    WO_PO_BREAK_DOWN wpbd ON wpdm.JOB_NO = wpbd.JOB_NO_MST
+JOIN 
+    LIB_BUYER lb ON wpdm.BUYER_NAME = lb.ID
+JOIN 
+    LIB_COMPANY lc ON wpdm.COMPANY_NAME = lc.ID
+LEFT JOIN 
+    LIB_BUYER_BRAND bb ON wpdm.BRAND_ID = bb.ID
+JOIN 
+    COM_SALES_CONTRACT csc ON wpdm.BUYER_NAME = csc.BUYER_NAME  
+JOIN 
+    COM_SALES_CONTRACT_ORDER_INFO cscoi ON csc.ID = cscoi.COM_SALES_CONTRACT_ID  
+JOIN 
+    COM_EXPORT_LC cel ON csc.INTERNAL_FILE_NO = cel.INTERNAL_FILE_NO
+WHERE 
+    wpdm.Is_deleted = 0
+    AND wpdm.status_active = 1
+    AND wpbd.Is_deleted = 0
+    AND wpbd.status_active = 1
+    AND lc.COMPANY_SHORT_NAME = 'MTSL'
+
+GROUP BY 
+    lc.COMPANY_SHORT_NAME, 
+    lb.SHORT_NAME,
+    bb.BRAND_NAME,
+    wpdm.JOB_NO,
+    csc.INTERNAL_FILE_NO,
+    csc.CONTRACT_NO,  
+    cscoi.ATTACHED_QNTY, cel.EXPORT_LC_NO,
+    cscoi.ATTACHED_VALUE
+ORDER BY 
+    wpdm.JOB_NO; 
